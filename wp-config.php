6ddfd33f11c40676f5feff7a60f446cd4610842a
kanhaiyasharma <kanhaiya.sharma@wwindia.com>
2016-03-02 15:44:36 +0530

    new theme added by timo

--- wp-config.php
+++ wp-config.php
@@ -20,15 +20,15 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'tradehouse');
+define('DB_NAME', 'wp789');
 
 /** MySQL database username */
-define('DB_USER', 'lead_sharad');
+define('DB_USER', 'root');
 
 define('FS_METHOD', 'direct');
 
 /** MySQL database password */
-define('DB_PASSWORD', 's_Q6W4&+d]JjWxh[');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
