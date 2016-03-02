b3995d154a1e2640ef7de1fb0de6fdc97d15ea2e
developer <developer@wwindia.com>
2016-03-02 15:30:27 +0530

    new theme added by timo name HTML5

---
+++ wp-content/themes/bestswiss/404.php
@@ -0,0 +1,31 @@
+<?php get_header(); ?>
+<!-- BODY -->
+
+<!-- SIDEBAR LEFT
+	<section id="sidebar" class="left">
+		<?php get_sidebar( 'left' ); ?>
+	</section> -->
+
+<!-- CONTENT -->
+<section id="sitecontent">
+
+	<!-- HEADLINE -->
+	<section class="spacer-1">
+		<h2 class="hidden">Spacer</h2>
+        <h1>Seite leider nicht gefunden</h1>
+    </section>
+
+    <!-- POST-INHALT 
+    <section id="content">
+    	<p><?php the_content();?></p>
+    </section> -->
+    
+
+</section>    
+
+<!-- SIDEBAR RIGHT
+	<section id="sidebar" class="right">
+		<?php get_sidebar( 'right' ); ?>
+	</section> -->
+
+<?php get_footer(); ?>
