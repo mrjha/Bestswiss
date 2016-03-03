15f31119b78879e991b95e1b3983115d2f206880
mrjha <monica.jha@wwindia.com>
2016-03-03 14:51:04 +0530

    Update functions.php

--- wp-content/themes/bestswiss/functions.php
+++ wp-content/themes/bestswiss/functions.php
@@ -230,7 +230,28 @@ INHALT
 		function woocommerce_support() {
     	add_theme_support( 'woocommerce' );}
     	
+/** menu active for custom taxonomy **/
+	add_action('parent_file', 'menu_correction');
+	function menu_correction($parent_file) 
+	{
+	global $current_screen;
+	$taxonomy = $current_screen->taxonomy;
 
-
+		if ($taxonomy == 'bstregions' )
+		{
+			$parent_file = 'edit-tags.php?taxonomy=bstregions&post_type=product';
+			return $parent_file;
+		}
+		if ($taxonomy == 'bstvencategory' )
+		{
+			$parent_file = 'edit-tags.php?taxonomy=bstvencategory&post_type=product';
+			return $parent_file;
+		}
+		else 
+		{
+			$parent_file = 'edit.php';
+			return $parent_file;
+		}
+	}
 
 ?>
